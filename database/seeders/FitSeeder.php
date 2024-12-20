4e436b84f933dda37d6059e70e04c9c42b427830
HtunHtunMin <tuntunmin2002@gmail.com>
2024-12-20 23:17:46 +0630

    seeder done

--- database/seeders/FitSeeder.php
+++ database/seeders/FitSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Fit;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class FitSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $fits  = [
+            ["user_id" => 1, 'fit' => 'Slim'],
+            ["user_id" => 1, 'fit' => 'Regular'],
+            ["user_id" => 1, 'fit' => 'Plus Size'],
+            ["user_id" => 1, 'fit' => 'XL'],
+            ["user_id" => 1, 'fit' => '2XL'],
+            ["user_id" => 1, 'fit' => '3XL'],
+        ];
+        Fit::insert($fits);
     }
 }
